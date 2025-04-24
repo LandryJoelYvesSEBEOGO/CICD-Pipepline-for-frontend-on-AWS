58d517c446c078d475d406cc8563199642b3bb8a
Landry <sebeogojoel@gmail.com>
2025-04-24 12:52:59 +0100

    a little cganes for testing the CICD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ const Footer = () => {
               variants={fadeIn('left', 1.0)}
               className="text-gray-600 text-sm"
             >
-              Created by Landry SEBEOGO
+              Created by Landry SEBEOGO Yves Joel
             </motion.p>
           </motion.div>
         </motion.div>
